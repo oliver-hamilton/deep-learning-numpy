Fixed gradients in convolutional layer

deeplearningnumpy/layers.py
@@ -133,7 +133,7 @@ class ConvolutionalLayer(Layer):
         assert filterDepth == imageDepth
 
         outImageSize = (imageSize - filterSize) // stride + 1
-        out = np.zeros((outImageSize, outImageSize))
+        out = np.zeros((outImageSize, outImageSize), dtype=np.float32)
         for outRow, maskedRow in enumerate(range(0, imageSize - filterSize + 1, stride)):
             for outCol, maskedColumn in enumerate(range(0, imageSize - filterSize + 1, stride)):
                 maskedImage = image[:, maskedRow:maskedRow+filterSize, maskedColumn:maskedColumn+filterSize]
@@ -168,15 +168,14 @@ class ConvolutionalLayer(Layer):
             #self.filterErrorGradient = self.convolution(self.inputs, dilatedDerivative, 1)
 
             #Rotate filters by 180 degrees
-            temp = np.rot90(nextLayer.weights, axes=(-1,-2))
-            rotatedFilters = np.rot90(temp, axes=(-1,-2))
+            rotatedFilters = np.rot90(nextLayer.weights, k = 2, axes=(-1,-2))
 
             #Pad the dilated output derivative
             paddedDerivative = np.pad(dilatedDerivative, ((0,0), (0,0), (rotatedFilters.shape[-2] - 1, rotatedFilters.shape[-2] - 1), (rotatedFilters.shape[-1] - 1, rotatedFilters.shape[-1] - 1)), 'constant', constant_values=(0))
-            errorSize = np.zeros(self.convolutionOutputs.shape)
+            errorSize = np.zeros(self.convolutionOutputs.shape, dtype=np.float32)
             for k in range(self.inputs.shape[0]):
                 for j in range(self.nOfFilters):
-                    errorSize[k, j] = sum([self.crossCorrelate(paddedDerivative[k, i], rotatedFilters[i, j], 1) for i in range(self.nOfFilters)])
+                    errorSize[k, j] = sum([self.crossCorrelate(paddedDerivative[k, i], rotatedFilters[i, j], 1) for i in range(nextLayer.nOfFilters)])
 
             
             #errorSize = self.crossCorrelate(paddedDerivative, rotatedFilters, 1)
@@ -198,7 +197,7 @@ class ConvolutionalLayer(Layer):
         #reshapedInputs = self.inputs.reshape(-1, self.inputs.shape[0], self.inputs.shape[2], self.inputs.shape[3])
         #The error gradient of the filter is the convolution of the dilated output derivative over the inputs
         #self.filterErrorGradient = self.convolution(self.inputs, dilatedDerivative, 1)
-        filterErrorGradient = np.zeros(self.weights.shape)
+        filterErrorGradient = np.zeros(self.weights.shape, dtype=np.float32)
         for i in range(self.nOfFilters):
             for j in range(self.previousNOfFilters):
                 #for i in range(self.nOfFilters):

deeplearningnumpy/models.py
@@ -62,7 +62,8 @@ class NeuralNetwork:
         for i in range(len(self.layers)-1, -1, -1):
             currentLayer = self.layers[i]
             #Reshape error gradients so that they can be added to the weights
-            currentLayer.updateWeights(self.errorGradientsSums[i], learningRate)
+            #TODO: Add back in
+            # currentLayer.updateWeights(self.errorGradientsSums[i], learningRate)
 
         #Make forward pass to get new network outputs that reflect updated weights
         self.forward(inputs)
@@ -128,24 +129,25 @@ class NeuralNetwork:
 
         #Iterate over each parameter in each layer
         for i in range(len(self.layers)):
-            estimatedGradients[i] = np.zeros(self.layers[i].weights.shape)
-            for j in range(len(self.layers[i].weights)):
-                for k in range(len(self.layers[i].weights[j])):
-                    #A small value so that the gradient can be calculated between 2 points
-                    EPSILON = 1e-4
-                    #Copy the weights matrix so that it can be replaced once gradient checking completes
-                    weightsCopy = self.layers[i].weights.copy()
-                    self.layers[i].weights[j][k] += EPSILON
+            flattenedWeights = self.layers[i].weights.reshape(-1)
+            estimatedGradients[i] = np.zeros(flattenedWeights.size)
+            for j in range(flattenedWeights.size):
+                #A small value so that the gradient can be calculated between 2 points
+                EPSILON = 1e-4
+                #Copy the weights matrix so that it can be replaced once gradient checking completes
+                weightsCopy = self.layers[i].weights.copy()
+                flattenedWeights[j] += EPSILON
 
-                    self.forward(inputs)
-                    gradPlus = costFunction.getCost(self.layers[-1].outputs, yReal)
-                    self.layers[i].weights[j][k] -= 2 * EPSILON
-                    self.forward(inputs)
-                    gradMinus = costFunction.getCost(self.layers[-1].outputs, yReal)
+                self.forward(inputs)
+                gradPlus = costFunction.getCost(self.layers[-1].outputs, yReal)
+                flattenedWeights[j] -= 2 * EPSILON
+                self.forward(inputs)
+                gradMinus = costFunction.getCost(self.layers[-1].outputs, yReal)
 
-                    estimatedGradients[i][j][k] = (gradPlus - gradMinus) / (2 * EPSILON)
+                estimatedGradients[i][j] = (gradPlus - gradMinus) / (2 * EPSILON)
 
-                    #Re-instate original layer weights
-                    self.layers[i].weights = weightsCopy
+                #Re-instate original layer weights
+                self.layers[i].weights = weightsCopy
+                flattenedWeights = self.layers[i].weights.reshape(-1)
 
         return estimatedGradients