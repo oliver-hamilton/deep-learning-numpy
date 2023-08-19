22a9d8dede88d7f6d27f86c1262e7aea1f65ae55
Oliver Hamilton <ohamilton0079@gmail.com>
2023-08-19 21:38:58 +0100

    Completed activation function documentation

--- deeplearningnumpy/activations.py
+++ deeplearningnumpy/activations.py
@@ -91,56 +91,164 @@ class ActivationLeakyReLU:
         return np.where(inputs > 0, 1, self._slope)
 
 class ActivationLinear:
-    """Linear activation function.
-    The derivative is 1 for all inputs.
-    """
+    """Linear activation function."""
     def forward(self, inputs):
-        """Propagates the inputs through the Linear activation, which has no effect"""
+        """Propagates the inputs through the Linear activation.
+        
+        Parameters
+        ----------
+        inputs : np.ndarray
+            The inputs to apply the activation function to.
+            All values remain unchanged.
+
+        Returns
+        -------
+        np.ndarray
+            The output of the activation function. 
+            The function is applied elementwise, and so the
+            output has the same dimensions as the input.
+        """
         return inputs
 
     def getDerivative(self, inputs):
-        """Gets the derivative of the activation function w.r.t the inputs."""
+        """Gets the derivative of the activation function w.r.t the inputs.
+        
+        Parameters
+        ----------
+        inputs : np.ndarray
+            The inputs to take the derivative with respect to.
+
+        Returns
+        -------
+        np.ndarray
+            The resulting derivatives. Since the activation is applied elementwise,
+            the result has the same dimensions as the input. All values are replaced
+            by 1.
+        """
         return np.ones(inputs.shape)
 
 class ActivationLogistic:
-    """Logistic activation function.
-    The derivative is logistic(x) * (1 - logistic(x))
-    """
+    """Logistic activation function."""
     def forward(self, inputs):
-        """Propagates the inputs through the Logistic activation."""
+        """Propagates the inputs through the Logistic activation.
+        
+        Parameters
+        ----------
+        inputs : np.ndarray
+            The inputs to apply the activation function to.
+            Each value ``x`` is replaced by ``1 / (1 + e^-x)``
+
+        Returns
+        -------
+        np.ndarray
+            The output of the activation function. 
+            The function is applied elementwise, and so the
+            output has the same dimensions as the input.
+        """
         return 1 / (1 + np.exp(-1 * (inputs)))
 
     def getDerivative(self, inputs):
-        """Gets the derivative of the activation function w.r.t the inputs."""
+        """Gets the derivative of the activation function w.r.t the inputs.
+        
+        Parameters
+        ----------
+        inputs : np.ndarray
+            The inputs to take the derivative with respect to.
+
+        Returns
+        -------
+        np.ndarray
+            The resulting derivatives. Since the activation is applied elementwise,
+            the result has the same dimensions as the input. All values ``x`` are replaced
+            by ``logistic(x) * (1 - logistic(x))``.
+        """
         logistic = self.forward(inputs)
         return logistic * (1 - logistic)
     
 class ActivationTanh:
-    """Tanh activation function.
-    The derivative is sech^2(x) = 1 - tanh^2(x)
-    """
+    """Tanh activation function."""
     def forward(self, inputs):
+        """Propagates the inputs through the Tanh activation.
+        
+        Parameters
+        ----------
+        inputs : np.ndarray
+            The inputs to apply the activation function to.
+            Each value ``x`` is replaced by ``(e^x - e^-x) / (e^x + e^-x)``
+
+        Returns
+        -------
+        np.ndarray
+            The output of the activation function. 
+            The function is applied elementwise, and so the
+            output has the same dimensions as the input.
+        """
         return (np.exp(inputs) - np.exp(-1 * inputs)) / (np.exp(inputs) + np.exp(-1 * inputs))
 
     def getDerivative(self, inputs):
+        """Gets the derivative of the activation function w.r.t the inputs.
+        
+        Parameters
+        ----------
+        inputs : np.ndarray
+            The inputs to take the derivative with respect to.
+
+        Returns
+        -------
+        np.ndarray
+            The resulting derivatives. Since the activation is applied elementwise,
+            the result has the same dimensions as the input. All values ``x`` are replaced
+            by ``sech^2(x) = 1 - tanh^2(x)``.
+        """
         tanh = self.forward(inputs)
         return 1 - tanh**2
 
 class ActivationSoftmax:
     """Softmax activation function.
+    
     The derivative is built into the cross-entropy loss, so we just
     assign a derivative of 1. The effect of this is that the softmax
     activation function can only be used with cross-entropy loss.
     """
     def forward(self, inputs):
-        """Propagates the inputs through the Softmax activation."""
+        """Propagates the inputs through the Softmax activation.
+        
+        Parameters
+        ----------
+        inputs : np.ndarray
+            The inputs to apply the activation function to.
+            Each value is divided by the sum of the values over
+            that last dimension.
+
+        Returns
+        -------
+        np.ndarray
+            The output of the activation function. 
+            This is a vector function that operates over the last dimension,
+            and the output has the same dimensions as the input.
+        """
         # Subtract max value in each batch to prevent overflow
+        # We can do this since softmax(x + c) = softmax(x)
         expValues = np.exp(inputs - np.max(inputs, axis=-1, keepdims=True))
-        # Normalise output values so values sum to 1
+        # Normalise exponential values so they sum to 1
         probabilities = expValues / np.sum(expValues, axis=-1, keepdims=True)
         return probabilities
 
     def getDerivative(self, inputs):
-        """Gets the derivative of the activation function w.r.t the inputs."""
+        """Gets the derivative of the activation function w.r.t the inputs.
+        
+        Parameters
+        ----------
+        inputs : np.ndarray
+            The inputs to take the derivative with respect to.
+
+        Returns
+        -------
+        np.ndarray
+            The resulting derivatives. Since the activation is applied elementwise,
+            the result has the same dimensions as the input. All values are replaced
+            by 1 since the true derivative is accounted for by the categorical cross-
+            entropy loss.
+        """
         return np.ones(inputs.shape)
 
